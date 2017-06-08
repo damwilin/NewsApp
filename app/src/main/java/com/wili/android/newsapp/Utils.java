348fdb20ddc6d9c7c57ff44cfb7e753552c0abaa
damwilin <damian.wilinski@gmail.com>
2017-06-08 15:20:51 +0200

    Added extractFeatureFromJson and public fetchData method

--- app/src/main/java/com/wili/android/newsapp/Utils.java
+++ app/src/main/java/com/wili/android/newsapp/Utils.java
@@ -1,6 +1,11 @@
 package com.wili.android.newsapp;
 
+import android.text.TextUtils;
 import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -11,8 +16,6 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 
-import static android.R.id.input;
-
 /**
  * Created by Damian on 6/7/2017.
  */
@@ -22,13 +25,16 @@ public class Utils {
 
     private Utils() {
     }
-    public static ArrayList<NewsItem> fetchData(String requestURL){
+
+    public static ArrayList<NewsItem> fetchData(String requestURL) {
         URL url = createURL(requestURL);
+        String jsonResponse = null;
         try {
-            String jsonResponse = makeHttpRequest(url);
+            jsonResponse = makeHttpRequest(url);
         } catch (IOException e) {
             Log.e(LOG_TAG, "Error creating jsonResponse", e);
         }
+        return extractFeatureFromJson(jsonResponse);
     }
 
     private static URL createURL(String requestURL) {
@@ -53,15 +59,14 @@ public class Utils {
             urlConnection.setConnectTimeout(15000);
             urlConnection.setRequestMethod("GET");
             urlConnection.connect();
-            if (urlConnection.getResponseCode() ==200){
+            if (urlConnection.getResponseCode() == 200) {
                 inputStream = urlConnection.getInputStream();
                 readFromStream(inputStream);
             }
         } catch (IOException e) {
             Log.e(LOG_TAG, "Problem retrieving the JSON Result", e);
-        }
-        finally {
-            if (urlConnection !=null)
+        } finally {
+            if (urlConnection != null)
                 urlConnection.disconnect();
             if (inputStream != null)
                 inputStream.close();
@@ -69,13 +74,13 @@ public class Utils {
         return jsonResponse;
     }
 
-    private static String readFromStream(InputStream inputStream) throws IOException{
+    private static String readFromStream(InputStream inputStream) throws IOException {
         StringBuilder output = new StringBuilder();
-        if (inputStream != null){
+        if (inputStream != null) {
             InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
             BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
             String line = bufferedReader.readLine();
-            while (line !=null){
+            while (line != null) {
                 output.append(line);
                 line = bufferedReader.readLine();
             }
@@ -83,9 +88,24 @@ public class Utils {
         return output.toString();
     }
 
-    private static ArrayList<NewsItem> parsingJson(String jsonResponse){
-        ArrayList<NewsItem> newsItems= new ArrayList<NewsItem>();
-
+    private static ArrayList<NewsItem> extractFeatureFromJson(String jsonResponse) {
+        ArrayList<NewsItem> newsItems = new ArrayList<NewsItem>();
+        if (TextUtils.isEmpty(jsonResponse))
+            return null;
+        try {
+            JSONObject root = new JSONObject(jsonResponse);
+            JSONArray results = root.getJSONArray("results");
+            for (int i = 0; i < results.length(); i++) {
+                JSONObject currResult = results.getJSONObject(i);
+                String title = currResult.optString("webTitle");
+                String date = currResult.optString("webPublicationDate");
+                String section = currResult.optString("sectionName");
+                URL articleURL = createURL(currResult.optString("webUrl"));
+                newsItems.add(new NewsItem(title, date, section, articleURL));
+            }
+        } catch (JSONException e) {
+            Log.e(LOG_TAG, "Problem parsing the json result", e);
+        }
         return newsItems;
     }
 }
